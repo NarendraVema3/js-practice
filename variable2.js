6ecaa29fb0479bb97e03675a13cc153a7f3d43fb
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 15:20:46 +0530

    Create variable2.js

---
+++ variable2.js
@@ -0,0 +1,20 @@
+
+var firstName = 'narendra';
+
+var lastName  = 'vema';
+
+console.log( firstname + lastName ) ;
+
+console.log( firstName . lastName ); 
+
+
+var job , isMarried
+
+job = software;
+
+isMarried = false;
+
+console.log('he is doing'.job.'is married person '.isMarried);
+
+
+
