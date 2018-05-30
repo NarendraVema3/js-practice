2d1fd0e5ce6d37ebc138d59eb4b8857740d09933
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 16:44:16 +0530

    Create booleanLogic.js

---
+++ booleanLogic.js
@@ -0,0 +1,19 @@
+
+var age = 24;
+
+if( age > 23 )
+{
+    elseif( age >24 && age < 30  )
+    {
+         elseif(age <26 || age > 28)
+         {
+            console.log(' her /gis age is 25 to 30 biddle os the age perosn');
+         }
+    }
+   }
+   else
+   {
+     console.log('his age was below 23');
+   }
+   
+   
