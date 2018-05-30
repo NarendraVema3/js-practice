d53ed04b37b8ca0cf78e58d48ec4804a15ee9313
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 18:24:51 +0530

    Create arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,16 @@
+
+var names = ['narendra','vema','venkata','sai'];
+
+
+var john = new array ('John', 'Smith', 1990, 'designer', false);
+
+
+john.push('blue');//enter a new object parameter at last
+
+john.unshift('mr')// enter a new object parameter at first 
+
+john.pop() // delete the data from the last  
+
+john.shift()// delate the data from the beging 
+
+console.log(john);
