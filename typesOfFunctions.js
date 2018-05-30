52076350491fb38df4027f505d8549947d804b92
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 17:31:53 +0530

    Create typesOfFunctions.js

---
+++ typesOfFunctions.js
@@ -0,0 +1,53 @@
+type 1 :-
+
+// function with out argument
+
+   function type1()
+   {
+     console.log('function with out argument');
+   }
+ 
+  type1();
+type 2:-
+
+ //function with argument
+ 
+    function type2(arg)
+    {
+      console.log('function with argument',arg);
+    }
+    
+    type2('arguments');
+    
+ type 3 :-
+ // void function
+      
+       void function type3()
+       {
+         console.log('this is void function');
+       }
+       
+       type3();
+  
+ type4 :-
+ 
+   // function return 
+   
+        function type4()
+         {
+            return true;
+         }
+
+       var typereturn = type4();
+       
+       
+          if(typereturn)
+          {
+             console.log('argument is true');
+          }
+          else
+          {
+             console.log("this argument is false");
+          }
+ 
+
