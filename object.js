8e234f1716eaafd7b434f727256e8ad7d701ec3b
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 19:11:00 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,27 @@
+
+var rohith = {
+           
+           FirstName = 'rohith',
+           LastName  =  'sharma',
+           age       =   30,
+           job       =  ' cricter ',
+           isMarried = true
+}
+
+
+console.log(rohith);
+
+
+rohith.wife = 'ritika';
+
+
+console.log(rohith.LastName);  // type 1
+
+
+console.log(rohith['LastName']); //type 2
+
+
+
+
+
+
