89b6dfe2d23f987c9d71eca1c42d77e7210c177b
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 15:40:37 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,32 @@
+
+ var dateOfBirth  = 1994;
+ 
+ var age = dateOfBirth - Date();
+ 
+ console.log(age);
+ 
+
+                                 clear explanation :-
+
+                                  var dateOfBirth  = 1994;
+ 
+                                  var age = dateOfBirth - 2016;
+ 
+                                  console.log(age);
+
+var ageNarendra = 30 ;
+
+var ageVema     = 30;
+
+var ageNarendra  =  ageVema = (3 + 6)*2 + 6;
+
+console.log(ageNarendra);
+
+console.log(ageVema);
+
+
+
+ 
+ 
+ 
+  
