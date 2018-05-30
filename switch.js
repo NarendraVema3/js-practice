252e088df24a932560a98f2c7e2892fe090dad5a
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 16:58:11 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,23 @@
+
+var job = prompt('which job your doing?');
+
+switch(job){
+ 
+    case 'teacher': 
+         console.log('he is a  teacher');
+         break;
+    case 'sfotware':
+          console.log('he is a software engineer');
+          break;
+    case 'hardware':
+          console.log('he is hardware engineer');
+          break;
+    case 'principal';
+          console.log('i am a principal');
+          break;
+   default:
+    console.log('not in the list');
+  
+}
+
+
