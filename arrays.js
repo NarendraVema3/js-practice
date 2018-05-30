0d58ab481066862c4b578bd393350d0b9d079d29
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 18:03:48 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,14 @@
+
+var names = ['narendra','vema','venkata','sai']
+var schl  = new array('sisumandhir','gurudhatta','prathiba','site');
+
+console.log(names[1]);
+
+names[0]='nari;
+
+console.log(names);
+
+
+
+console.log(schl);'
+
