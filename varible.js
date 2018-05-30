7de252760ec8e2179b1a82d4794156d3aab203fd
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 15:11:37 +0530

    Create varible.js

---
+++ varible.js
@@ -0,0 +1,44 @@
+var FirstName = 'narendra';
+
+console.log(FirstName);
+
+var LastName = 'vema';
+
+console.log(FirstName,LastName);
+
+var number = 9989166691;
+
+console.log(nummber);
+
+var gender = male ;
+
+console.log(gender);
+
+
+
+
+EXPLANATION :-
+ 
+   1)  In java script var behaves like a data type and it's a global variable 
+   2)  all the above variables are innitialized variables 
+        
+         what is the meaning of innitialized varibles and declarative variables ?
+         
+         A) -> decarative variable is nothig but the is just declare the varible type
+             
+              Example:-
+              //  below example belongs to declaration process
+              
+              var FirstName = String;
+              
+             -> initialization is nothing but the innitiate satine value to the to the varibale immediatle 
+                
+                Example:- 
+                
+                // below exaple is for the innitialize variable 
+                
+                 var number = 9989166691 ;// number type 
+                 
+                 
+                 
+                 
