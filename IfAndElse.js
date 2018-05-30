2bc705b7eb27eafe768ece1866141a20b2c0f70d
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 16:13:40 +0530

    Create IfAndElse.js

---
+++ IfAndElse.js
@@ -0,0 +1,62 @@
+
+if(true)
+{
+  console.log('this is the true block');
+}
+else
+{
+  console.log('this is the false block');
+}
+
+ 
+ var name = 'narendra';
+ 
+ if(name != 'narendra')
+ {
+    console.log('yes i am not narendra');  
+ }
+else
+{
+  console.log('no i am narendra');
+ }
+ 
+ 
+ 
+ var isMarried = false ;
+ 
+ if(isMarried)
+ {
+    console.log('yes i am married');
+ }
+ else
+ {
+ console.log('no i am unMarried');
+ }
+
+ var age = 24;
+ 
+ 
+ if(age==='24')
+ {
+     console.log('the value is true :)');
+ }
+ else
+ {
+   console.log('the value is false :@' );
+ }
+ 
+ 
+                                      Q) do you know the differeance between == and === ?
+                                        A) ->  '===' expecting same type of data type varible 
+                                            Example :- 
+                                               if(24 === '24') // false 
+                                               
+                                               
+                                           ->  '==' expecting the same data that's it   
+                                                if(24 == '24') // true
+                                                
+                                               
+                                               
+                                               
+ 
+ 
