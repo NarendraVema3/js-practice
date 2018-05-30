2ffc7750c1da77857d9129b7d1f77e29c3ceddc8
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-30 19:26:01 +0530

    Create object2.js

---
+++ object2.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+var rohit = new Object();
+
+rohit.firstName = 'rohith';
+rohit.Lastname  = ' sharma';
+rohit.age       = 30;
+rohit.job       =" indian cricter";
+
+
+console.log(rohit);
+
+
+
