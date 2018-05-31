8d6005a27e52b9cfcd6e9f8d2fc7a90b37d4df2e
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-31 14:31:29 +0530

    Create for loops.js

---
+++ for loops.js
@@ -0,0 +1,32 @@
+
+for ( i = 0 ; i < 10; i++)
+{
+   
+    console.log(i);
+
+}
+
+output :-
+  0
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+
+ var mumbaiIndians = ['suryakumar','lewis','rohith','ishanKishan','hardik','krunal','pollard','benCutting','markende','bumrah','akhila']
+ 
+ for ( i = 0; i <= 11 ;i++ )
+ {
+    console.log(mumbaiIndians[i]);
+ }
+
+
+for ( i = mumbaiIndians.length ; i >= 0 ; i--  )
+{
+    console.log( mumbaiIndians[i] );
+}
