3f2f0369419bed5367da740c83df0e11769873c1
NarendraVema3 <36954999+NarendraVema3@users.noreply.github.com>
2018-05-31 14:02:17 +0530

    Create objectsAndMethods.js

---
+++ objectsAndMethods.js
@@ -0,0 +1,34 @@
+
+var mumbaiIndians = {
+  
+                         TeamName:'MubaiIndians',
+                         owner   : ' reliance industries',
+                         started : 2008,
+                         trophys :function(year)
+                         {
+                            if( year === 2013 || year === 2015 || year === 2017 )
+                            {
+                               return rohith;
+                            }
+                            else
+                            {
+                              console.log('not listed'); 
+                            } 
+                         }
+
+
+
+}
+
+var capitan = trophy(2013);
+
+var capitan = trophy(2015);
+
+var capitan = trophy(2017);
+
+
+console.log(mumbaiIndians.'won the trophys in the captancy of '.capitan);
+
+ 
+
+
